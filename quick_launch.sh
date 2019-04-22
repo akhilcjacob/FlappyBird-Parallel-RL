2eec0f5e7074fae294baf7161ae7915ec5d2767e
akhil <akhilcjacob@gmail.com>
2019-04-21 20:20:41 -0400

    Fixed launch script added more output

--- quick_launch.sh
+++ quick_launch.sh
@@ -1,13 +1,19 @@
-python flappy.py 1 > output/1_agent & disown
+echo "Launching 1 agent..."
+nohup python flappy.py 1 > output/1_agent & 
 sleep 4s
 
-python flappy.py 4 > output/4_agent & disown
+echo "Launching 4 agents..."
+nohup python flappy.py 4 > output/4_agent & 
 sleep 4s
 
-python flappy.py 8 > output/8_agent & disown
+echo "Launching 8 agents..."
+nohup python flappy.py 8 > output/8_agent & 
 sleep 4s
 
-python flappy.py 20 > output/20_agent & disown
+echo "Launching 20 agents..."
+nohup python flappy.py 20 > output/20_agent &
 sleep 4s
 
-sleep 120m && pkill -9 python
+echo "Launching kill 2h kill command..."
+sleep 120m && pkill -9 python &
+echo "Done..."
